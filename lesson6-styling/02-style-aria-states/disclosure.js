3c4c27b5b951d0c5920eb2d9f844d5a00cd2977a
Sandra Marquez <dnrvarela@yahoo.com>
2018-05-14 18:33:11 -0500

    aria styling

--- lesson6-styling/02-style-aria-states/disclosure.js
+++ lesson6-styling/02-style-aria-states/disclosure.js
@@ -13,17 +13,18 @@ function toggleDisclosure(e) {
   }
 
   e.preventDefault();
-
+//checking to see if content is displayed
   if (content.getAttribute('aria-hidden') === 'true') {
-
+//if it's not set the area hidden to false on the content
     content.setAttribute('aria-hidden', 'false');
+    //apply the classes to indicate wether they are showing or not
     content.classList.remove('hidden');
-
+//and aria-expanded to true on the button to indicate that the widget is open
     button.setAttribute('aria-expanded', 'true');
     button.classList.add('expanded');
 
   } else {
-
+//or if the content is displaying it will do the opposite
     content.setAttribute('aria-hidden', 'true');
     content.classList.add('hidden');
 
